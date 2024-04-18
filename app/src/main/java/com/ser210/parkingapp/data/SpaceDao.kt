5d13fc04fd017b09a8b64dfbd2f7ecdd557d28cf
atoomer <aarondtoomer@gmail.com>
2024-04-18 18:53:00 -0400

    updated some of the data/view model classes. Pushing to safely pull fragments from master

--- app/src/main/java/com/ser210/parkingapp/data/SpaceDao.kt
+++ app/src/main/java/com/ser210/parkingapp/data/SpaceDao.kt
@@ -8,11 +8,20 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface SpaceDao {
     @Update
-    suspend fun updateSpace(space: Space)
+    suspend fun update(space: Space)
+
+
+    // parking database update. Function that updates the student id and filled status of a space based on lot name and space id.
+    @Query("UPDATE spaces SET student_id = :studentId, space_full = :spaceFull WHERE id = :spaceId AND lot_name = :lotName")
+    suspend fun updateSpaceStudentIdPark(studentId: Int, spaceFull: Boolean, spaceId: Int, lotName: String)
+
+
+    @Query("SELECT * FROM spaces")
+    fun getAllSpaces(id: Int) : Flow<Space>
 
     @Query("SELECT * FROM spaces WHERE lot_name = :lotName")
     fun getSpacesByLot(lotName: String): Flow<Space>
 
     @Query("SELECT * FROM spaces WHERE student_id = :studentId")
-    fun getSpacesByStudent(studentId: Int): Flow<List<Space>>
+    fun getSpaceByStudent(studentId: Int): Space
 }
--- app/src/main/java/com/ser210/parkingapp/data/SpaceRoomDatabase.kt
+++ app/src/main/java/com/ser210/parkingapp/data/SpaceRoomDatabase.kt
@@ -6,18 +6,22 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Space::class], version = 1, exportSchema = false)
-abstract class SpaceRoomDatabase: RoomDatabase() {
+abstract class SpaceRoomDatabase : RoomDatabase() {
 
     abstract fun spaceDao(): SpaceDao
 
-    companion object{
+    companion object {
 
         @Volatile
         private var INSTANCE: SpaceRoomDatabase? = null
 
-        fun getDatabase(context: Context): SpaceRoomDatabase{
-            return INSTANCE ?: synchronized( this ){
-                val instance = Room.databaseBuilder(context.applicationContext, SpaceRoomDatabase::class.java, "spaces")
+        fun getDatabase(context: Context): SpaceRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SpaceRoomDatabase::class.java,
+                    "spaces_database"
+                )
                     .fallbackToDestructiveMigration()
                     .build()
                 INSTANCE = instance
