05f184ef28f21f389ab3204d8b2f4d808fb0f3e8
AJArmy60 <aidanjarmellino@gmail.com>
2024-05-01 00:52:23 -0400

    Nav Drawer attempt

--- app/src/main/java/com/ser210/parkingapp/MainActivity.kt
+++ app/src/main/java/com/ser210/parkingapp/MainActivity.kt
@@ -10,15 +10,30 @@ import androidx.navigation.fragment.NavHostFragment
 import com.ser210.parkingapp.data.Space
 import com.ser210.parkingapp.data.SpaceRoomDatabase
 import com.ser210.parkingapp.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
